11571e5287bac3b300ddb04300ee2b6aaac822a5
YoungWookCho <impact0823@naver.com>
2016-11-24 11:00:06 +0900

    바뀜

--- src/com/hanbit/ForExercise.java
+++ src/com/hanbit/ForExercise.java
@@ -5,7 +5,46 @@ public class ForExercise {
 	public static void main(String[] args) {
 		
 		
+		// 1. 1부터 10까지의 합을 구하는 함수를 작성하세요.
+		System.out.println(getSumOneToTen());
 		
+		// 2. 두개의 정수를 입력받아
+		//    첫번째 수 부터 두번째 수까지의 합을
+		//    구하는 함수를 작성하세요.
+		int from = 10, to = 30;
+		System.out.println(getSum(from, to));
+		
+		
+		
+	}
+
+	static int getSum(int from, int to) {
+		// TODO Auto-generated method stub
+		
+		int iCount=0;
+		int iSum=0;
+		
+		for ( iCount=from; iCount<=to; iCount++){
+			iSum+=iCount;
+		}
+		
+		
+		return iSum;
+		
+	
+	}
+
+	static int getSumOneToTen() {
+		// TODO Auto-generated method stub
+		int iCount=0;
+		int iSum=0;
+		
+		for ( iCount=1; iCount<=10; iCount++){
+			iSum+=iCount;
+		}
+		
+		
+		return iSum;
 	}
 
 }
